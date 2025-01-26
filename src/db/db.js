07fde927353f9f02f484400a25996d193f950ac0
”EvanO3” <evan.oni@hotmail.com>
2025-01-25 23:15:10 -0500

    added src/ directory

---
+++ src/db/db.js
@@ -0,0 +1,9 @@
+const createClient = require("@supabase/supabase-js")
+require("dotenv").config()
+const supabaseURL = process.env.supabaseURL;
+const supabaseKey = process.env.supabaseKey
+const supabase =createClient(supabaseURL,supabaseKey)
+
+module.exports=supabase
+
+
