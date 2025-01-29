82f2a5e11a3096a39daf264680afc2fd91f512cf
”EvanO3” <evan.oni@hotmail.com>
2025-01-28 21:35:32 -0500

    added db logic to connect to supabase

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,12 @@
-const createClient = require("@supabase/supabase-js")
+const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config()
 const supabaseURL = process.env.supabaseURL;
-const supabaseKey = process.env.supabaseKey
+const supabaseKey = process.env.supabaseKey;
+
 const supabase =createClient(supabaseURL,supabaseKey)
 
 module.exports=supabase
+
+
 
 
