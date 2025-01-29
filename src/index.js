e23f962f8218bfdcc73cc5acfed0810ab14e6ffb
”EvanO3” <evan.oni@hotmail.com>
2025-01-28 21:35:57 -0500

    added routes for application usage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
-const PORT =process.env.PORT || 4000
+const routes = require("./routes/Routes")
+
+
+const PORT =process.env.PORT || 3000
+app.use(express.json())
+app.use(routes)
+
+
+
+
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
