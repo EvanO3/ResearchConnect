6b8c85026b2b3d3efb51d349fa0cd91ae87a6699
”EvanO3” <evan.oni@hotmail.com>
2025-02-01 01:51:00 -0500

    added a check to see if the db was successfully connected when the app is launched

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-const express = require("express")
-const app = express()
-require("dotenv").config()
-const routes = require("./routes/Routes")
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const routes = require("./routes/Routes");
+const supabase = require("./db/connection.js");
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(` Server is running and Listening on port ${PORT}`);
+});
 
 
-const PORT =process.env.PORT || 3000
-app.use(express.json())
-app.use(routes)
-
-
-
-
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}`)
+(async ()=>{
+    try{
+        const {data , error} = await supabase.rpc("now");
+        if(error){
+            console.error("Database connection error:", error.message)
+        }else{
+            console.log("Database successfully connected")
+        }
+    }catch(error){
+        console.error('Internal server error', error)
+    }
 })
-
-
-
-
-
-
