22efbc5efca4c0649b5cceb881c1e2d8a16fb11a
”EvanO3” <evan.oni@hotmail.com>
2025-03-14 18:46:19 -0400

    initial commit

---
+++ src/main/java/com/researchconnect/ResearchConnect/ResearchConnectApplication.java
@@ -0,0 +1,13 @@
+package com.researchconnect.ResearchConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/researchconnect/ResearchConnect/ResearchConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.researchconnect.ResearchConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
